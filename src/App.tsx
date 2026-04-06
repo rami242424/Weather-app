95a213a902d704d96c666a8df3d26553da9c78af
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-06 17:11:39 +0900

    first-commit(setting)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+type State = {
+  city: string;
+  weather : any;
+  loading: boolean;
+  error: string | null;
+}
+
+function App(){
+  return (
+    <>
+      <span>날ㅆㅣ앱</span>
+    </>
+  );
+}
+
+export default App;
