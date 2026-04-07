795185d4a586854706dd85fcf7a484c78a6285aa
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-07 17:17:55 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { useReducer } from "react";
 type State = {
   city : string;
   loading : boolean;
-  weather : any;
+  weather : Weather | null;
   error: string | null;
+}
+
+type Weather = {
+  name: string;
+  temp: number;
 }
 const initialState : State = {
   city: "",
@@ -20,15 +25,68 @@ type Action =
 
 function reducer (state: State, action: Action) : State{
   switch(action.type){
+    case "SET_CITY":
+      return {
+        ...state,
+        city: action.payload
+      }
+    case "FETCH_START":
+      return {
+        ...state,
+        loading: true,
+        error: null
+      }
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        weather: action.payload
+      }
+    case "FETCH_ERROR":
+      return {
+        ...state,
+        loading: true,
+      }
+
     default :
     return state;
   }
 }
 function App(){
   const [state, dispatch] = useReducer(reducer, initialState);
+  const fetchWeather = async() => {
+    dispatch({ type: "FETCH_START"});
+
+    try {
+      const fakeData = {
+        name: "Seoul",
+        temp: 20
+      };
+
+      setTimeout(() => {
+        dispatch({ type: "FETCH_SUCCESS", payload: fakeData});
+      }, 1000);
+
+    }
+    catch (err) {
+      dispatch({ type: "FETCH_ERROR", payload: "에러발생"});
+    }
+  };
+
+
   return (
     <>
-      <input />
+      <input value={state.city} onChange={(e) => dispatch({ type: "SET_CITY", payload: e.target.value})} />
+      <button onClick={fetchWeather}>Search</button>
+      {state.loading 
+        ? "...loading" 
+        : state.weather && (
+          <div>
+            <h3>{state.weather.name}</h3>
+            <p>{state.weather.temp}°C</p>
+          </div>
+        )
+      }
     </>
   );
 }
