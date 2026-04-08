98d385bb88c7976f285b9cb811149f6fb6963ab8
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-08 10:48:18 +0900

    weather app start with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
-// SET_CITY
-// FETCH_START
-// FETCH_SUCCESS
-// FETCH_ERROR
-
-import { useReducer } from "react";
-
-type State = {
-  city: string;
-  weather: Weather | null;
-
-}
-
-type Weather = {
-  name: string;
-  temp: number;
-}
-
-type Action = 
-  | { type: "SET_CITY" }
-
-  
-const initialState = {
-  city: "",
-  weather: null
-}
-
-function reducer (state:State, action:Action) : State {
-  switch(action.type){
-
-  default:
-  return state;
-}
-
-}
-
 function App(){
-  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
-      <input />
+      <input/>
       <button>Search</button>
     </>
-  );
+  )
 }
-
 
 export default App;
