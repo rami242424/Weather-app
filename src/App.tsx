fd61a9076098b5b69700a3fd55da9c6a198146a9
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-08 09:57:47 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,48 @@
+// SET_CITY
+// FETCH_START
+// FETCH_SUCCESS
+// FETCH_ERROR
+
 import { useReducer } from "react";
 
 type State = {
-  city : string;
-  loading : boolean;
-  weather : Weather | null;
-  error: string | null;
+  city: string;
+  weather: Weather | null;
+
 }
 
 type Weather = {
   name: string;
   temp: number;
 }
-const initialState : State = {
+
+type Action = 
+  | { type: "SET_CITY" }
+
+  
+const initialState = {
   city: "",
-  loading: false,
-  weather: null,
-  error: null
+  weather: null
 }
-type Action =
-  | { type: "SET_CITY", payload: string }
-  | { type: "FETCH_START" }
-  | { type: "FETCH_SUCCESS"; payload: any }
-  | { type: "FETCH_ERROR"; payload: string }
 
-function reducer (state: State, action: Action) : State{
+function reducer (state:State, action:Action) : State {
   switch(action.type){
-    case "SET_CITY":
-      return {
-        ...state,
-        city: action.payload
-      }
-    case "FETCH_START":
-      return {
-        ...state,
-        loading: true,
-        error: null
-      }
-    case "FETCH_SUCCESS":
-      return {
-        ...state,
-        loading: false,
-        weather: action.payload
-      }
-    case "FETCH_ERROR":
-      return {
-        ...state,
-        loading: true,
-      }
 
-    default :
-    return state;
-  }
+  default:
+  return state;
 }
+
+}
+
 function App(){
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const fetchWeather = async() => {
-    dispatch({ type: "FETCH_START"});
-
-    try {
-      const fakeData = {
-        name: "Seoul",
-        temp: 20
-      };
-
-      setTimeout(() => {
-        dispatch({ type: "FETCH_SUCCESS", payload: fakeData});
-      }, 1000);
-
-    }
-    catch (err) {
-      dispatch({ type: "FETCH_ERROR", payload: "에러발생"});
-    }
-  };
-
-
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
-      <input value={state.city} onChange={(e) => dispatch({ type: "SET_CITY", payload: e.target.value})} />
-      <button onClick={fetchWeather}>Search</button>
-      {state.loading 
-        ? "...loading" 
-        : state.weather && (
-          <div>
-            <h3>{state.weather.name}</h3>
-            <p>{state.weather.temp}°C</p>
-          </div>
-        )
-      }
+      <input />
+      <button>Search</button>
     </>
   );
 }
+
 
 export default App;
