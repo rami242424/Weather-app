62b84f331ed29821677bd02ec3194ed3f2e6bf84
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-08 13:51:36 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function App(){
+  const [inputValue, setInputValue] = useState("");
+  const inputChange = () => {
+    
+  }
   return (
     <>
-      <input/>
+      <input value={inputValue} onChange={inputChange} placeholder="도시의 이름을 입력하세요"/>
       <button>Search</button>
     </>
   )
