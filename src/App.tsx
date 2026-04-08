0ad9c2bcb414768042cc9afba1220e9838906331
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-08 14:38:59 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
+// typing -> 입력완료하면 state에 저장해야함
+// loading
+// success -> weather data
+// fail -> error msg
 import { useState } from "react";
 
 function App(){
-  const [inputValue, setInputValue] = useState("");
-  const inputChange = () => {
-    
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [weather, setWeather] = useState<string[] | null>(null);
+  
+  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
   }
+  const searchBtn = async() => {
+    // 기본설정
+    setLoading(true);
+    setError(false);
+    setWeather(null);
+
+    // api요청
+    try {
+      const response = await fetch(`api주소`);
+      const json = await response.json();
+      setWeather(json);
+
+    } catch(error) {
+      //
+
+    }
+  }
+
   return (
     <>
-      <input value={inputValue} onChange={inputChange} placeholder="도시의 이름을 입력하세요"/>
-      <button>Search</button>
+      <input value={city} onChange={inputChange} placeholder="도시의 이름을 입력하세요."/>
+      <button onClick={searchBtn}>Search</button>
+      {loading ? "...Loading" 
+        : {weather ? {weather} : {error}}
+      }
     </>
   )
 }
