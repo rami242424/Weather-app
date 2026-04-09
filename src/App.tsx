29783a796d4766e789908f6c83b4c1ad6e283dee
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-09 17:01:40 +0900

    commit a

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
 import { useState } from "react";
 
 type Weather = {
-  name : string;
-  temp : number;
+  name: string;
+  temp: number;
 }
+
 type Props = {
-  loading: boolean;
-  error: boolean;
-  weather : Weather | null;
-}
-function WeatherContent({loading,error,weather}:Props){
-  if(loading) return <div>Loading...</div>
-  if(error) return <div>Error!!</div>
-  if(weather) return <>
-    <h2>도시 이름 : {weather.name}</h2>
-    <h2>온도 : {weather.temp}º</h2>
-  </>
-  return null;
+  weather : Weather | null
 }
 
 function App(){
   const [city, setCity] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState<Weather|null>(null);
   const [error, setError] = useState(false);
-  const [weather, setWeather] = useState<Weather | null>(null);
-  
+  const [loading, setLoading] = useState(false);
   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
   }
@@ -32,35 +21,29 @@ function App(){
     setLoading(true);
     setError(false);
     setWeather(null);
-
-    const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
-
     try{
+      const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       if(!response.ok) throw new Error();
       const json = await response.json();
       setWeather({
         name: json.name,
-        temp: json.main.temp
+        temp: json.main.temp,
       });
-    } catch (error) {
+
+    } catch(error) {
       setError(true);
-      console.log(error, "에러발생");
+      console.log(error, "catch error 발생")
     } finally {
       setLoading(false);
     }
   }
   return (
     <>
-      <input value={city} onChange={inputChange} placeholder="도시의 이름을 입력하세요."/>
+      <input placeholder="도시이름을 입력하세요." value={city} onChange={inputChange}/>
       <button onClick={searchBtn}>Search</button>
-      <WeatherContent
-        loading={loading}
-        error={error}
-        weather={weather}
-      />
     </>
-  )
+  );
 }
 
 export default App;
