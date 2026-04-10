a1ff5842474f5f85c9a7685d6cc90434bf36ca23
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-10 11:38:21 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import { useState } from "react";
 
+type Weather = {
+  name: string;
+  temp: number;
+}
+
 
 function App(){
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string|null>(null);
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState<Weather|null>(null);
   const [city, setCity] = useState("");
   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
   }
+  const searchBtn = async() => {
+    setLoading(true);
+    setError(null);
+    setWeather(null);
+
+    const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${API_KEY}&units=metric`);
+      if(!response.ok) throw new Error();
+      const json = await response.json();
+      setWeather({
+        name: json.name,
+        temp: json.main.temp,
+      })
+    } catch(error:any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <>
       <input value={city} onChange={inputChange} placeholder="도시이름을 입력하세요."/>
-      <button>Search</button>
+      <button onClick={searchBtn}>Search</button>
     </>
   );
 }
