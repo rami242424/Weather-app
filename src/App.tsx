dab6af1ce715c4b5c3aea7d9a083f6a580003ecf
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-10 15:59:00 +0900

    reducer fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 type Weather = {
   name: string;
@@ -6,54 +6,136 @@ type Weather = {
 }
 
 
-function App(){
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string|null>(null);
-  const [weather, setWeather] = useState<Weather|null>(null);
-  const [city, setCity] = useState("");
-  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setCity(e.target.value);
+type State = {
+  loading : boolean,
+  error: string | null,
+  weather: Weather | null,
+  city: string
+}
+
+type Action = 
+  | { type: "INPUT_CHANGE"; payload: string}
+  | { type: "START_SEARCH" }
+  | { type: "FETCH_SUCCESS"; payload: Weather}
+  | { type: "FETCH_FAIL"; payload: string}
+
+
+const initialState:State = {
+  loading : false,
+  error: null,
+  weather: null,
+  city: ""
+}
+
+const reducer = (state:State, action:Action):State => {
+  switch(action.type){
+    case "INPUT_CHANGE":
+      return {
+        ...state,
+        city: action.payload
+      }
+    case "START_SEARCH":
+      return {
+        ...state,
+        loading:true,
+        error:null,
+        weather:null
+      }
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          loading: false,
+          error: null,
+          weather: action.payload
+        }
+        case "FETCH_FAIL":
+          return {
+            ...state,
+            loading: false,
+            error: action.payload,
+            weather: null
+          }
+    default:
+      return state;
   }
+}
+
+function App(){
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState<string|null>(null);
+  // const [weather, setWeather] = useState<Weather|null>(null);
+  // const [city, setCity] = useState("");
+  // const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  //   setCity(e.target.value);
+  // }
   const searchBtn = async() => {
-    if(!city.trim()) return;
-    setLoading(true);
-    setError(null);
-    setWeather(null);
+    //START_SEARCH------------------------------
+    if(!state.city.trim()) return;
+
+    dispatch({type: "START_SEARCH"});
+    // setLoading(true);
+    // setError(null);
+    // setWeather(null);
 
     const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+
+    //FETCH_SUCCESS------------------------------
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${API_KEY}&units=metric`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${state.city.trim()}&appid=${API_KEY}&units=metric`);
       if(!response.ok){
         if(response.status === 404){
-          throw new Error("1");
+          throw new Error("도시를 찾을 수 없습니다.");
         } else {
-          throw new Error("2");
+          throw new Error("서버 오류가 발생했습니다.");
         }
       }
       const json = await response.json();
-      setWeather({
-        name: json.name,
-        temp: json.main.temp,
-      })
+
+      dispatch({
+        type:"FETCH_SUCCESS",
+        payload: {
+          name: json.name,
+          temp: json.main.temp,
+        }
+      });
+      // setWeather({
+      //   name: json.name,
+      //   temp: json.main.temp,
+      // })
+      
+      //FETCH_FAIL------------------------------------------------
     } catch(error) {
-      //console.log("전체:", error);
       if (error instanceof Error) {
-        //console.log("메시지:", error.message);
-        setError(error.message);
+        dispatch({
+          type: "FETCH_FAIL",
+          payload: error.message
+        });
       }
-    } finally {
-      setLoading(false);
-    }
+
+
+    } 
+    // finally {
+    //   setLoading(false);
+    // }
   }
   return (
     <>
-      <input value={city} onChange={inputChange} placeholder="도시이름을 입력하세요."/>
-      <button onClick={searchBtn}>Search</button>
-      {loading && <div>Loading...</div>}
-      {error && <div>{error}</div>}
-      {weather && <div>
-          <h3>도시이름 : {weather.name}</h3>
-          <h3>온도 : {Math.ceil(weather.temp)}°C</h3>
+      <input 
+        value={state.city} 
+        onChange={(e:React.ChangeEvent<HTMLInputElement>) => dispatch({type: "INPUT_CHANGE", payload: e.target.value })} 
+        placeholder="도시이름을 입력하세요."
+      />
+      <button 
+        onClick={searchBtn}
+      >
+        Search
+      </button>
+      {state.loading && <div>Loading...</div>}
+      {state.error && <div>{state.error}</div>}
+      {state.weather && <div>
+          <h3>도시이름 : {state.weather.name}</h3>
+          <h3>온도 : {Math.ceil(state.weather.temp)}°C</h3>
         </div>
       }
     </>
