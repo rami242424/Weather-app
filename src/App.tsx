9735f9972cfd4dc08fceb13163b9312dea53b11c
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-13 11:39:16 +0900

    reducer again dr

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ type State = {
 }
 
 type Action =
-| { type: "INPUT_CHANGE" };
+  | { type: "INPUT_CHANGE", payload: string }
+  | { type: "START_SEARCH" }
 
 const initialState = {
   city: "",
@@ -34,8 +35,18 @@ function App(){
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      <input/>
-      <button>Search</button>
+      <input 
+        value={state.city} 
+        onChange={(e:React.ChangeEvent<HTMLInputElement>) => dispatch({ type: "INPUT_CHANGE", payload: e.target.value })}
+      />
+      <button onClick={() => dispatch({ type: "START_SEARCH" })}>Search</button>
+      {state.loading && <div>Loading...</div>}
+      {state.error && <div>Error:{error}</div>}
+      {state.weather && <div>
+          <h3>도시이름 : {state.weather.name}</h3>
+          <h3>온도 : {Math.ceil(state.weather.temp)}°C</h3>
+        </div>
+      }
     </>
   );
 }
