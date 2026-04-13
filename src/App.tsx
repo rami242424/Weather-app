2e1ddc2ca7eb9286ae49e7ea9116f89310421b87
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-13 19:01:27 +0900

    again

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useReducer } from "react";
 
+type State = {
+  loading: boolean;
+  error : string | null;
+  weather : Weather | null;
+  city : string;
+};
+
 type Weather = {
   name: string;
   temp: number;
-}
-
-type State = {
-  city: string;
-  loading: boolean;
-  weather: Weather | null;
-  error: string | null;
-}
-
-type Action =
-  | { type: "INPUT_CHANGE", payload: string }
-  | { type: "SEARCH_START" }
-  | { type: "SEARCH_SUCCESS", payload: Weather}
-  | { type: "SEARCH_FAIL", payload: string}
+};
 
 const initialState = {
-  city: "",
-  loading: false,
+  loading: true,
   error: null,
   weather: null,
-}
+  city: "",
+};
 
-const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+type Action = 
+  | { type: "INPUT_CHANGE"; payload: string }
+  | { type: "SEARCH_START" }
+  | { type: "SEARCH_SUCCESS"; payload: Weather }
+  | { type: "SEARCH_FAIL"; payload: string }
 
-function reducer(state:State, action:Action):State{
+
+function reducer(state:State, action:Action):State {
   switch(action.type){
     case "INPUT_CHANGE":
       return {
@@ -39,60 +38,56 @@ function reducer(state:State, action:Action):State{
         ...state,
         loading: true,
         error: null,
-        weather: null
+        weather: null,
       }
     case "SEARCH_SUCCESS":
       return {
         ...state,
         loading: false,
         error: null,
-        weather: action.payload
+        weather: action.payload,
       }
     case "SEARCH_FAIL":
       return {
         ...state,
         loading: false,
         error: action.payload,
-        weather: null
+        weather: null,
       }
     default:
       return state;
   }
-}  
+}
 
+const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
 
 function App(){
   const [state, dispatch] = useReducer(reducer, initialState);
   const getWeather = async() => {
-    if(!state.city.trim()){
-      dispatch({
-        type: "SEARCH_FAIL",
-        payload: "도시명을 입력하세요."
-      });
-      return;
-    }
-
-    // 초기화
+    if(!state.city.trim()) return;
+    //초기화
     dispatch({ type: "SEARCH_START" });
 
-    try{
+    try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${state.city.trim()}&appid=${API_KEY}&units=metric`);
       if(!response.ok){
         if(response.status === 404){
-          throw new Error ("도시이름을 찾을 수 없습니다.");
+          throw new Error("도시 이름을 찾을 수 없습니다.");
         } else {
-          throw new Error ("서버에 에러가 발생했습니다.");
+          throw new Error("서버에 연결 할 수 없습니다.");
         }
       }
+
       const json = await response.json();
 
       dispatch({
-        type:"SEARCH_SUCCESS", 
+        type: "SEARCH_SUCCESS",
         payload: {
           name: json.name,
-          temp: json.main.temp
-      }});
-    } catch(error) {
+          temp: json.main.temp,
+        }
+      });
+    } catch(error){
       if(error instanceof Error){
         dispatch({
           type: "SEARCH_FAIL",
@@ -100,65 +95,26 @@ function App(){
         });
       }
     }
-  };
-  const getLocationWeather = () => {
-    dispatch({
-      type: "SEARCH_START"
-    });
-    navigator.geolocation.getCurrentPosition(
-      async(position) => {
-        // 성공
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
+  }
+  const getCurrentLocation = () => {
 
-        try {
-          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
-          if(!response.ok) throw new Error("위치 기반 날씨 조회 실패");
-          const json = await response.json();
-          
-          dispatch({
-            type: "SEARCH_SUCCESS",
-            payload: {
-              name: json.name,
-              temp: json.main.temp
-            }
-          })
-        } catch (error) {
-          if( error instanceof Error){
-            dispatch({ 
-              type: "SEARCH_FAIL",
-              payload: error.message
-            });
-          }
-        }
-      },
-
-
-      (error) => {
-        dispatch({
-          type: "SEARCH_FAIL",
-          payload: error.message
-        })
-      }
-    );
   }
   return (
     <>
-      <input 
-        value={state.city} 
-        onChange={(e:React.ChangeEvent<HTMLInputElement>) => dispatch({ type: "INPUT_CHANGE", payload: e.target.value })}
-        placeholder="도시명을 입력하세요."
-      />
-      <button onClick={getWeather} disabled={state.loading}>Search</button>
-      <button onClick={getLocationWeather} disabled={state.loading}>Current Location</button>
-      {state.loading && <div>Loading...</div>}
-      {state.error && <div>{state.error}</div>}
-      {state.weather && (
-        <div>
-          <h3>도시이름 : {state.weather.name}</h3>
-          <h3>온도 : {Math.ceil(state.weather.temp)}°C</h3>
-        </div>
-      )}
+    <input 
+      value={state.city} 
+      placeholder="도시이름을 입력해주세요." 
+      onChange={(e:React.ChangeEvent<HTMLInputElement>) => dispatch({ type: "INPUT_CHANGE", payload: e.target.value})}
+    />
+    <button onClick={getWeather}>Search</button>
+    <button onClick={getCurrentLocation}>My Current Location</button>
+    {state.loading && <div>Loading...</div>}
+    {state.error && <div>{state.error}</div>}
+    {state.weather&& <div>
+        <h3>도시이름 : {state.weather.name}</h3>
+        <h3>온도 : {Math.ceil(state.weather.temp)}°C</h3>
+      </div>
+    }
     </>
   );
 }
