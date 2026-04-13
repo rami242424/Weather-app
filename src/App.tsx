f21f09c6bbecda95229928591d785d22e2b923a5
Rami <138556019+rami242424@users.noreply.github.com>
2026-04-13 11:50:10 +0900

    reducer again dr

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,43 @@ type State = {
 type Action =
   | { type: "INPUT_CHANGE", payload: string }
   | { type: "START_SEARCH" }
+  | { type: "FETCH_SUCCESS", payload: Weather}
+  | { type: "FETCH_FAIL", payload: string}
 
 const initialState = {
   city: "",
-  loading: true,
+  loading: false,
   error: null,
   weather: null,
 }
 
 function reducer(state:State, action:Action):State{
   switch(action.type){
+    case "INPUT_CHANGE":
+      return {
+        ...state,
+      }
+    case "START_SEARCH":
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        weather: null
+      }
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        weather: action.payload
+      }
+    case "FETCH_FAIL":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        weather: null
+      }
     default:
       return state;
   }
@@ -41,7 +68,7 @@ function App(){
       />
       <button onClick={() => dispatch({ type: "START_SEARCH" })}>Search</button>
       {state.loading && <div>Loading...</div>}
-      {state.error && <div>Error:{error}</div>}
+      {state.error && <div>Error:{state.error}</div>}
       {state.weather && <div>
           <h3>도시이름 : {state.weather.name}</h3>
           <h3>온도 : {Math.ceil(state.weather.temp)}°C</h3>
